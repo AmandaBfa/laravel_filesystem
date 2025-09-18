853a5e116ca7353cf1196dacb48789aad7044da4
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-09-18 15:22:33 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<x-main-layout>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <p class="text-center display-3">Laboratorio de FileSystem</p>
+            </div>
+        </div>
+    </div>
+
+</x-main-layout>
